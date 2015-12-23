76262e235db6bc1321058f2c804c50a66ba4f56a
Mike McGarr <mmcgarr@netflix.com>
2015-12-23 14:10:40 -0500

    Incorrectly parsing project names that include .git in the name, like a *.github.io repository

--- main_test.go
+++ main_test.go
@@ -14,6 +14,7 @@ func TestScenarios(t *testing.T) {
 	AssertEquals(t, "github/sstephenson/bats", ParseRepo("git@github.com:sstephenson/bats.git"))
 	AssertEquals(t, "github/sstephenson/bats", ParseRepo("https://github.com/sstephenson/bats.git"))
 	AssertEquals(t, "github/jmcgarr/bats", ParseRepo("https://github.com/JMCGARR/bats"))
+	AssertEquals(t, "github/nebula-plugins/nebula-plugins.github.io", ParseRepo("git@github.com:nebula-plugins/nebula-plugins.github.io.git"))
 	AssertEquals(t, "stash/users/jmcgarr/homedir", ParseRepo("ssh://git@stash.funville.com:1234/~jmcgarr/homedir.git"))
 	AssertEquals(t, "stash/users/jmcgarr/homedir", ParseRepo("http://jmcgarr@stash.funville.com/scm/~jmcgarr/homedir.git"))
 	AssertEquals(t, "stash/hackday/template", ParseRepo("ssh://git@stash.funville.com:7999/hackday/template.git"))
